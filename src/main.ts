55c9340ca790306f5857e3a482caeaaa7c76dd1d
JuanScaparro <juanscaparro@gmail.com>
2022-05-04 19:20:27 -0300

    Feat: eval() / complete function

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,55 @@
-import { add, divide, multiply, percent, substract } from './operations';
 import './style.css';
 
 
-let actualValue: string = '';
-let termA: number = 0;
-let termB: number = 0;
+let actualValue: string = '0';
 let operator: string = '';
 let isResult: boolean = false;
 
+const screen: HTMLDivElement = <HTMLDivElement>document.getElementById( 'display' );
+const operation: HTMLDivElement = <HTMLDivElement>document.getElementById( 'operation' );
 const bOperators: NodeListOf<HTMLButtonElement> = document.querySelectorAll('.operator');
 const bNumbers: NodeListOf<HTMLButtonElement> = document.querySelectorAll( '.number' );
-const screen: HTMLDivElement = <HTMLDivElement>document.getElementById( 'display' );
 const bClear: HTMLButtonElement = <HTMLButtonElement>document.getElementById( 'clear' );
 const bBack: HTMLButtonElement = <HTMLButtonElement>document.getElementById( 'back' );
 const bEqual: HTMLButtonElement = <HTMLButtonElement>document.getElementById( 'equal' );
+const bPercent: HTMLButtonElement = <HTMLButtonElement>document.getElementById( 'percent' );
 
 bClear.addEventListener( 'click', clear );
 bBack.addEventListener( 'click', backSpace );
 bEqual.addEventListener( 'click', equal );
-bNumbers.forEach( item => item.addEventListener( 'click', () => inputNumber( (item.innerHTML).trim() ) ));
-bOperators.forEach( item => item.addEventListener( 'click', () => inputOperator( (item.innerHTML).trim() ) ));
+bPercent.addEventListener( 'click', percent );
+bNumbers.forEach( item => item.addEventListener( 'click', () => inputNumber( ( item.innerHTML ).trim() ) ));
+bOperators.forEach( item => item.addEventListener( 'click', () => inputOperator( ( item.innerHTML ).trim() ) ));
 
 function display(): void {
   screen.innerHTML = actualValue;
 };
 
+function displayOperation(): void {
+  operation.innerHTML = actualValue + '=';
+};
+
+function clearDisplayOperation(): void {
+  operation.innerHTML = '';
+};
+
 function inputNumber( item: string ): void {
-  if( isResult && !operator.length ){
-    clear();
-  }
-  if( item === '.' && actualValue.includes( '.' )) return
-  actualValue = actualValue + item;
+  if( isResult && !operator.length ) clear();
+  if( item === '.' && actualValue.includes( '.' )) return;
+  checkZeroes( item );
+  actualValue = actualValue === '0' ? item : actualValue + item;
   display();
 };
 
+function checkZeroes( item: string ): void {
+  const isOperatorZero: boolean = actualValue.endsWith( operator + '0' );
+  if( isOperatorZero && item !== '.' ) {
+    actualValue = actualValue.slice( 0, -1 );
+  };
+};
+
 function inputOperator( item: string ): void {
+  if( isResult ) clearDisplayOperation();
   operator = item;
   actualValue = actualValue + item;
   isResult = false;
@@ -42,21 +57,22 @@ function inputOperator( item: string ): void {
 };
 
 function clear(): void {
-  actualValue = '';
+  actualValue = '0';
   operator = '';
   isResult = false;
   display();
+  clearDisplayOperation();
 };
 
 function backSpace(): void {
+  if(isResult) {
+    clear();
+    return;
+  }
   actualValue = actualValue.slice( 0, -1 );
   display();
 };
 
-function operation( numA:  number, numB: number, fnOp: Function ): void {
-  actualValue = fnOp( numA, numB );
-  showResult();
-};
 
 function showResult(): void {
   operator = '';
@@ -65,33 +81,21 @@ function showResult(): void {
 };
 
 function equal(): void {
-  if( !operator.length || isResult ) return;
-  // actualValue = eval(actualValue)
-  const terms: string[] = actualValue.split( operator );
-  if ( terms.length === 1 ) return;
-  termA = Number( terms[0] );
-  termB = Number( terms[1] );
-  operation( termA, termB, getFnOperation() );
+  if( actualValue.length < 3 || isResult ) return;
+  displayOperation();
+  actualValue = eval( actualValue );
+  showResult();
 };
 
-function getFnOperation(): Function {
-  let fn: Function = add;
-  switch( operator ) {
-    case '+': 
-            fn = add;
-            break;
-    case '-':
-            fn = substract;
-            break;
-    case '*': 
-            fn = multiply;
-            break;
-    case '/':
-            fn = divide;
-            break;
-    case '%':
-            fn = percent;
-            break;
-  };
-  return fn;
+function percent(): void {
+  if( actualValue.length < 3 || isResult ) return;
+  displayOperation();
+  actualValue = String( eval( actualValue )/100 );
+  showResult();
 };
+
+function init(): void {
+  display();
+};
+
+init();
